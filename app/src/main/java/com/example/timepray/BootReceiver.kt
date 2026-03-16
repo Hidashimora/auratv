a55129944e436355068ed2999034052541f1d757
irkarimov <ibnrinat02@gmail.com>
2026-03-16 17:31:24 +0400

    relese v3 add new link

--- app/src/main/java/com/example/timepray/BootReceiver.kt
+++ app/src/main/java/com/example/timepray/BootReceiver.kt
@@ -7,12 +7,17 @@ import android.content.Intent
 import android.util.Log
 
 class BootReceiver : BroadcastReceiver() {
+    companion object {
+        const val EXTRA_STARTED_FROM_BOOT = "com.example.timepray.extra.STARTED_FROM_BOOT"
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
             Intent.ACTION_BOOT_COMPLETED,
             Intent.ACTION_LOCKED_BOOT_COMPLETED -> {
 
                 val launchIntent = Intent(context, MainActivity::class.java).apply {
+                    putExtra(EXTRA_STARTED_FROM_BOOT, true)
                     // создаём новый таск, очищаем старый
                     addFlags(
                         Intent.FLAG_ACTIVITY_NEW_TASK or
